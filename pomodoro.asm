7575478bdb34d090ec88c50bf5329a73fc192ece
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-09-17 11:58:03 +0100

    Add (main): run file with a little bit of program structure.

---
+++ pomodoro.asm
@@ -0,0 +1,11 @@
+global _start
+
+; this program is meant to be a pomodoro timer written in assembly
+; so let's see how it goes.
+section .data
+
+section .text
+_start:
+  MOV eax, 4
+
+section .exit
